4d7bbf8be55a0833bafed1a61169294aecc97625
MARLADEEPIKA04 <marladeepika410@gmail.com>
2025-12-05 21:32:25 +0530

    Add files via upload

---
+++ password_genertor.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+def generate_password(length):
+    # Available character combinations
+    characters = string.ascii_letters + string.digits + string.punctuation
+    
+    # Randomly choose characters to create password
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+# Taking user input
+try:
+    length = int(input("Enter password length: "))
+    
+    if length < 4:
+        print("Password length should be at least 4 characters!")
+    else:
+        print("Generated Password:", generate_password(length))
+
+except ValueError:
+    print("Please enter a valid number!")
